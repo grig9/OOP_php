3250570a98ae4ad60ea5434c26e21f0e7fbe367b
grig9 <grigorevalex91@gmail.com>
2022-01-25 19:24:21 +0300

    Update QueryBuilder.php

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -3,22 +3,24 @@
 class QueryBuilder {
   protected $pdo;
 
-  public function __construct($pdo) {
+  public function __construct($pdo) 
+  {
     $this->pdo = $pdo;
   }
 
-  public function getAll($table) {
+  public function getAll($table) 
+  {
     $sql = "SELECT * FROM {$table}";
     $statement = $this->pdo->prepare($sql);
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
   }
 
-  public function create($table, $data) {
+  public function create($table, $data) 
+  {
     $keys = implode(',', array_keys($data));
     $tags = ":" . implode(', :', array_keys($data));
-    $sql = "INSERT INTO {$table} ($keys) VALUES ($tags)";
-    // var_dump($sql);die;
+    $sql = "INSERT INTO $table ($keys) VALUES ($tags)";
     $statement = $this->pdo->prepare($sql);
     $statement->execute($data);
   }
